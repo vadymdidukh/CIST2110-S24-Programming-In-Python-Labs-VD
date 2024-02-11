7f22b3b8974cb9a034ca15dbe595771416ba1dde
vadymdidukh <didukhv@go.stockton.edugit confi --global user.name>
2024-02-11 15:25:01 -0500

    Finised labs

--- Lab2.py
+++ Lab2.py
@@ -1,29 +1,33 @@
 # Lab2
-# Author:
+# Author: Vadym Didukh
 
 """_summary_
 This lab is designed to get you familiar with Python Data Types, Variables, and Arithmetic Operators.
 """
 
 # 1. Create a variable called "name" and assign it the value of your name. Print the variable.
-
+name = "Vadym Didukh"
+print(name) 
 # 2. Create a variable called "age" and assign it the value of your age. Print the variable.
-
+age = 35 
+print(age)
 # 3. Create a variable called "favorite_color" and assign it the value of your favorite color. Print the variable.
-
+favorite_color = "Blue" 
+print(favorite_color)
 # 4. Create a variable called "favorite_movie" and assign it the value of your favorite movie. Print the variable.
-
+favorite_movie = "The Equalizer"
+print(favorite_movie)
 # 5. Use string concatenation to print the following sentence: "Hello, my name is <name>. I am <age> years old. My favorite color is <favorite_color> and my favorite movie is <favorite_movie>."
 # Note: punctuation and capitalization matters. Make sure your sentence matches the one above exactly.
-
+print("Hello, my name is " + name +". I am " + str(age) +" years old. My favorite color is " + favorite_color + " and my favorite movie is " + favorite_movie + ".")
 # 6. Create a variable called "my_age_in_dog_years" and assign it the value of your age multiplied by 7.
-
+my_age_in_dog_years = age * 7
 # 7. Create a variable called "days_in_a_year" and assign it the value of 365.
-
+days_in_a_year = 365
 # 8. Create a variable called "days_alive" and assign it the value of your age multiplied by the number of days in a year.
-
+days_alive = age * days_in_a_year
 # 9. Create a variable called "days_alive_in_dog_years" and assign it the value of your age in dog years multiplied by the number of days in a year.
-
+days_alive_in_dog_years = my_age_in_dog_years * days_in_a_year
 # 10. Create a chart that shows the following information:
 # - Your name
 # - Your age
@@ -33,8 +37,16 @@ This lab is designed to get you familiar with Python Data Types, Variables, and 
 # - Number of days in a year
 # - Number of days you have been alive
 # - Number of days you have been alive in dog years
-
-
+print("==================================")
+print("| Name: " + name + "             |")
+print("| Age: " + str(age) + "                        |")
+print("| Favorite Color: " + favorite_color + "           |")
+print("| Favorite Movie: " + favorite_movie + "  |")
+print("| Age in Dog Years: " + str(my_age_in_dog_years) + "          |") 
+print("| Days in a Year: " + str(days_in_a_year)  + "            |")
+print("| Days Alive: " + str(days_alive) +"              |")
+print("| Days Alive in Dog Years: " + str(days_alive_in_dog_years) + " |")
+print("==================================")
 """
 Make it look pretty as well as informative. Use the following format:
 ==================================
--- Lab3.py
+++ Lab3.py
@@ -1,25 +1,27 @@
 # Lab3
-# Author:
+# Author: Vadym Didukh
 
 """_summary_
 This lab is designed to get you familiar with Python input(), converting data types, and using the round() function.
 """
 
 # 1. Ask the user for their name and assign it to a variable called "name".
-
+name = input("What is your name? ")
 # 2. Ask the user for their age and assign it to a variable called "age".
-
+age = input("What is your age? ")
 # 3. Ask the user for a balance and assign it to a variable called "balance".
-
+balance = float(input("What is your balance? "))
+print(balance)
 # 4. Ask the user for a number of years to calculate interest and assign it to a variable called "years".
-
+years = input("How many years would youlike to calculate interest for? ")
 # 5. Ask the user for an interest rate and assign it to a variable called "interest_rate".
-
+interest_rate = float(input("What is your interest rate? "))
 # 6. Convert the balance, years, and interest_rate to float data types.
 # Note: This is only necessary if you did not specify the data type in the input() function.
-
+years = float(years)
 # 7. Calculate the future balance using the formula: balance * (1 + interest_rate/100) ** years
-
+future_balance = balance * (1 + interest_rate / 100) **years
 # 8. Round the future balance to 2 decimal places using the round() function.
-
+future_balance = round(future_balance, 2)
 # 9. Print the following sentence using string concatenation: "Hello <name>, your balance after <years> years will be $<future_balance>."
+print("Hello " + name + ", your balance after " + str(years) + " years will be $" + str(future_balance) + ".")
