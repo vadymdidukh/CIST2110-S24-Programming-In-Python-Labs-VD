1ba8ceeb55f8585ab48349ab4f0163152ad190c9
vadymdidukh <didukhv@go.stockton.edugit confi --global user.name>
2024-02-17 14:55:41 -0500

    Finished lab

--- Lab1.py
+++ Lab1.py
@@ -5,7 +5,7 @@
 # 1. Uncomment the following print statement and run the program. Show the output to your instructor.
 # Note: to "uncomment a line", remove the # at the beginning of the line. The shortcut is to select the line and press Ctrl + / (windows) or Command + / (mac)
 #Note2: to run the program, Select the "Play" button at the top of the screen.
-print("Hello World")
+print("Hello Worl")
 
 # 2. Add a print statement that prints your name. Run the program and show the output to your instructor.
 print("Vadym Didukh")
